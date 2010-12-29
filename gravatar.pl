cd7164d751483ffeab0ce8689289cb1018382038
c9s <cornelius.howl@gmail.com>
2010-12-30 01:40:18 +0800

    gravatar fetcher

---
+++ gravatar.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/env perl
+use LWP::Simple qw(getstore);
+use Digest::MD5 qw(md5_hex);
+%users = qw( 
+    audreyt audreyt@audreyt.org
+    clkao clkao@clkao.org
+    c9s cornelius.howl@gmail.com
+    gugod gugod@gugod.org
+);
+
+map { 
+    warn $_->[0];
+    getstore("http://www.gravatar.com/avatar/" . $_->[1] , 'output/' . $_->[0] . ".png")
+    } map { [ $_ , md5_hex($users{$_}) ] } keys %users;
