029350f3ec327e49a7be6ae6390b1f64fbba0f7b
c9s <cornelius.howl@gmail.com>
2010-12-30 13:00:53 +0800

    seperate face image dir

--- gravatar.pl
+++ gravatar.pl
@@ -10,7 +10,8 @@ use Digest::MD5 qw(md5_hex);
     clsung clsung@gmail.com
 );
 
+$outdir = 'output/faces';
 map { 
     warn $_->[0];
-    getstore("http://www.gravatar.com/avatar/" . $_->[1] , 'output/images/' . $_->[0] . ".png")
+    getstore("http://www.gravatar.com/avatar/" . $_->[1] , 'output/faces/' . $_->[0] . ".png")
     } map { [ $_ , md5_hex($users{$_}) ] } keys %users;
